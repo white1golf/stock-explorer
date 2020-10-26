b0e40bdef9b6a79e34bb96b9bbc6d3e77a5dc8d3
san <changsan2438@gmail.com>
2020-10-26 14:46:20 +0900

    feat : authMixin 을 App.vue에 그대로 구현.
    Mixin으로 구현을 하다보니 this binding 이나 closer variable에 대한
    고려가 필요해서 그냥 편하게 App.vue 의 data, methods, life cycle hooks
    에 맞게 변경하여 적용함.

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,137 @@
 </template>
 
 <script>
+let interval;
+
 export default {
   name: 'App',
 
+  data() {
+    return {
+      inMemoryToken: {},
+      jwtToken: '',
+      jwtTokenExpiry: '',
+      userId: '',
+      refreshToken: '',
+      refreshTokenExpiry: '',
+    };
+  },
+
   methods: {
+    async login({ jwt_token, jwt_token_expiry }, noRedirect) {
+      this.inMemoryToken = {
+        token: jwt_token,
+        expiry: jwt_token_expiry,
+      };
+      if (!noRedirect) {
+        this.$router.push('/갈곳');
+      }
+    },
+    async logout() {
+      this.inMemoryToken = null;
+      const response = await fetch('/api/logout', {
+        method: 'POST',
+        credentials: 'include',
+      });
+
+      window.localStorage.setItem('logout', Date.now());
+      this.$router.push('/login');
+    },
+
+    subMinutes(dt, minutes) {
+      return new Date(dt.getTimes() - minutes * 60 * 1000);
+    },
     //위로가기 기능.
     scrollTo(hash) {
       location.href = hash;
     },
+
+    async auth() {
+      if (Object.keys(this.inMemoryToken).length == 0) {
+        const url = 'api/refresh-token';
+        try {
+          const response = await fetch(url, {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+              'Content-Type': 'application/json',
+              'Cache-Control': 'no-cache',
+            },
+            body: JSON.stringify({}),
+          });
+          if (response.status === 200) {
+            const {
+              jwt_token,
+              refresh_token,
+              jwt_token_expiry,
+              expiry,
+              refresh_token_expiry,
+              user_id,
+            } = await response.json();
+            await this.login({ jwt_token, jwt_token_expiry }, true);
+          } else {
+            let error = new Error(response.statusText);
+            error.response = response;
+            throw error;
+          }
+        } catch (error) {
+          this.$router.push('/login');
+        }
+      }
+
+      const jwt_token = this.inMemoryToken;
+      if (!jwt_token) {
+        this.$router.push('/login');
+      }
+      return jwt_token;
+    },
+
+    syncLogout(event) {
+      if (event.key === 'logout') {
+        console.log('logged out from storage!');
+        this.$router.push('/login');
+      }
+    },
   },
 
-  mounted() {
+  async created() {
+    const token = await this.auth();
+    if (!this.inMemoryToken) {
+      this.inMemoryToken = token;
+    }
+  },
+
+  async mounted() {
+    interval = setInterval(async () => {
+      //1. 이미 토큰을 가지고 있을 때.
+      if (this.inMemoryToken) {
+        if (
+          this.subMinutes(new Date(this.inMemoryToken.expiry), 1) <=
+          new Date(this.inMemoryToken.expiry)
+        ) {
+          this.inMemoryToken = {};
+          const token = await this.auth();
+          this.inMemoryToken = token;
+        }
+      } else {
+        const token = await this.auth();
+        this.inMemoryToken = token;
+      }
+    }, 60000);
+
+    window.addEventListener('storage', this.syncLogout);
+
     //해쉬로 가기?
     if (this.$route.hash) {
+      //모든 vue instance 들이 mounted 됨을 보장하기 위해 nextTick 사용.
       this.$nextTick(() => this.scrollTo(this.$route.hash));
     }
+  },
+
+  beforeUnmout() {
+    clearInterval(interval);
+    window.removeEventListener('storage', this.syncLogout);
+    window.localStorage.remoteItem('logout');
   },
 };
 </script>
