88f964ac8c7ed07184479881796ab943293c59aa
san <changsan2438@gmail.com>
2020-11-02 17:04:52 +0900

    Feature : 메뉴 아이템 추가. 아바타 및 드랍다운 메뉴.

---
+++ src/components/Profile.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="profile">
+    <div @click="isMenuActive = !isMenuActive">
+      <Avatar
+        class="avatar"
+        :size="40"
+        username="changsan2438@gmail.com"
+      ></Avatar>
+    </div>
+    <div class="dropdown-menu" :class="{ 'is-active': isMenuActive }">
+      <router-link to="/setting" class="menu-item">
+        설정
+      </router-link>
+      <a class="logout menu-item" @click="logout">
+        로그아웃
+      </a>
+    </div>
+  </section>
+</template>
+
+<script>
+import Avatar from 'vue-avatar';
+import Constant from '../store/constant';
+
+export default {
+  components: {
+    Avatar,
+  },
+  data() {
+    return {
+      isMenuActive: false,
+    };
+  },
+  computed: {
+    isAuthenticated() {
+      return this.$store.state.isAuthenticated;
+    },
+    userName() {
+      return this.$store.state.user.user_id;
+    },
+  },
+  methods: {
+    closemenu() {
+      this.isMenuActive = false;
+    },
+    async logout() {
+      await this.$store.dispatch(Constant.LOGOUT);
+      window.localStorage.setItem('logout', Date.now());
+      if (this.$router.currentRoute.path === '/') {
+        this.$router.go();
+      } else this.$router.push('/');
+    },
+  },
+  mounted() {
+    this.$eventHub.$on('navigate', this.closeMenu);
+  },
+};
+</script>
